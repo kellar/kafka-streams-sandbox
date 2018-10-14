f6a952ab946fc1d57f410758064e7196bfc009d5
kellar <blakekellar@gmail.com>
2018-10-13 19:08:38 -0500

    Initialize multi-module gradle project, start.spring.io

---
+++ transformer/src/main/kotlin/com/blakekellar/sandbox/kafkastreams/kafkastreamssandbox/KafkastreamssandboxApplication.kt
@@ -0,0 +1,11 @@
+package com.blakekellar.sandbox.kafkastreams.kafkastreamssandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkastreamssandboxApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkastreamssandboxApplication>(*args)
+}
---
+++ transformer/src/test/kotlin/com/blakekellar/sandbox/kafkastreams/kafkastreamssandbox/KafkastreamssandboxApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.blakekellar.sandbox.kafkastreams.kafkastreamssandbox
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KafkastreamssandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
